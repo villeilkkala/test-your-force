b2bb32c6737f8ebc0e16c2e876a845a90dc5471e
Olli Niskanen <olli.niskanen@futurice.com>
2015-11-21 00:37:21 +0200

    Webcam working

---
+++ Assets/Webcam.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using UnityEngine.UI;
+
+
+public class Webcam : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		WebCamDevice[] devices = WebCamTexture.devices;
+		for( var i = 0 ; i < devices.Length ; i++ )
+		    Debug.Log(devices[i].name);
+
+		WebCamTexture texture = new WebCamTexture (devices[0].name);
+		texture.Play();
+
+		this.GetComponent<RawImage>().texture = texture;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
