9b12c7936ad39fccd304b9aafb544d7fa8e1dded
Ville Ilkkala <ville.ilkkala@gmail.com>
2015-11-21 19:08:57 +0200

    added sound prefab

---
+++ Assets/soundThing.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(AudioSource))]
+public class ExampleClass : MonoBehaviour
+{
+    void Start()
+    {
+        AudioSource audio = GetComponent<AudioSource>();
+        audio.Play();
+        audio.Play(44100);
+    }
+}
